e45ede8a0cd34413ec9b2b1905cf17c84237de5d
vguillosson <vguillosson@est.frba.utn.edu.ar>
2020-09-11 18:09:24 -0300

    Estructuras de Sindicato

--- Sindicato/src/sindicatoLib.h
+++ Sindicato/src/sindicatoLib.h
@@ -24,6 +24,44 @@ typedef struct{
 
 }sindicato_config;
 
+//chequear las estructuras, en muchas seguro se pasan a t_list*
+typedef struct{
+
+	int cantidadCocineros;
+	int posicionX;
+	int posicionY;
+	char* afinidadCocineros;
+	char* platos;
+	int precioPlatos;
+	int cantidadHornos;
+
+}info_restaurante;
+
+typedef struct{
+
+	char* estadoPedido;
+	char* listaPlatos;
+	int cantidadPlatos;
+	int cantidadLista;
+	int precioTotal;
+
+}pedidos;
+
+typedef struct{
+
+	char* pasos;
+	int tiempoPasos;
+
+}receta;
+
+typedef struct{
+
+	info_restaurante infoRestaurante;
+	pedidos pedidosRestaurante;
+	receta recetaRestaurante;
+}restaurante; // no se si es correcto hacer esto, de ultima se saca despues
+
+
 sindicato_config* sindicatoStruct;
 t_log* logger;
 
